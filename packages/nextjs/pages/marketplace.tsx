f49c297e4a3ef59002a2a4a986bc86104d0dbff4
Clément <clab60917@eleve.isep.fr>
2023-12-30 19:50:43 +0100

    add popup marketplace

--- packages/nextjs/pages/marketplace.tsx
+++ packages/nextjs/pages/marketplace.tsx
@@ -81,28 +81,54 @@ const nfts = [
     price: "12.0 ETH",
   },
 ];
-const NFTCard = ({ nft }) => (
-  <div className="border border-gray-300 rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-shadow duration-300 ease-in-out p-4 bg-white dark:bg-gray-800">
-    {/* Styles en ligne pour fixer la largeur à 250px et la hauteur à 300px */}
-    <img src={nft.image} alt={`NFT ${nft.id}`} style={{ width: "250px", height: "300px", objectFit: "cover" }} />
-    <div className="p-4">
-      <h2 className="font-semibold text-xl text-gray-900 dark:text-gray-100">{nft.title}</h2>
-      <p className="text-gray-700 dark:text-gray-300">{nft.description}</p>
-      <div className="flex justify-between items-center mt-4">
-        <span className="font-semibold text-gray-900 dark:text-gray-100">{nft.price}</span>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded transition duration-200">
-          Buy
-        </button>
+const NFTCard = ({ nft }) => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleBuyClick = () => {
+    setShowPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
+  return (
+    <div className="relative border border-gray-300 rounded-lg overflow-hidden shadow-md hover:shadow-xl transition-shadow duration-300 ease-in-out p-4 bg-white dark:bg-gray-800">
+      <img src={nft.image} alt={`NFT ${nft.id}`} style={{ width: "250px", height: "300px", objectFit: "cover" }} />
+      <div className="p-4">
+        <h2 className="font-semibold text-xl text-gray-900 dark:text-gray-100">{nft.title}</h2>
+        <p className="text-gray-700 dark:text-gray-300">{nft.description}</p>
+        <div className="flex justify-between items-center mt-4">
+          <span className="font-semibold text-gray-900 dark:text-gray-100">{nft.price}</span>
+          <button
+            onClick={handleBuyClick}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded transition duration-200"
+          >
+            Buy
+          </button>
+        </div>
       </div>
+      {showPopup && (
+        <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-10">
+          <div className="bg-white p-4 rounded-lg shadow-lg">
+            <p>NFTs are not currently available, come back later!</p>
+            <button
+              onClick={handleClosePopup}
+              className="mt-4 bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 const Marketplace = () => {
   const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    // Mettre à jour la classe de la balise HTML en fonction du thème
     document.documentElement.classList.toggle("dark", darkMode);
   }, [darkMode]);
 
